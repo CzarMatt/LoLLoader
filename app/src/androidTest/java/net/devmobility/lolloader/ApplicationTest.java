a53bfd1004bee5a0768861ef785109641a07e519
Matt Mossman <czarmatt@gmail.com>
2015-05-08 04:36:49 -0700

    Added a junit test case.

--- app/src/androidTest/java/net/devmobility/lolloader/ApplicationTest.java
+++ app/src/androidTest/java/net/devmobility/lolloader/ApplicationTest.java
@@ -1,13 +1,55 @@
 package net.devmobility.lolloader;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
+import android.app.FragmentTransaction;
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.suitebuilder.annotation.SmallTest;
+import android.widget.ImageButton;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
+public class ApplicationTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    private MainActivityFragment fragment;
+    private MainActivity activity;
+
     public ApplicationTest() {
-        super(Application.class);
+        super(MainActivity.class);
     }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        activity = getActivity();
+
+        fragment = new MainActivityFragment();
+
+        FragmentTransaction transaction = activity.getFragmentManager().beginTransaction();
+        transaction.replace(R.id.main_container, fragment, "main");
+        transaction.commit();
+
+    }
+
+    // Validate the intent is fired on button press with correct result from
+    // text field
+    @SmallTest
+    public void testJsonResponse() {
+
+        final ImageButton button = (ImageButton) activity.findViewById(R.id.imagebutton);
+        assertNotNull("This image button shouldn't be null", button);
+        activity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                button.performClick();
+            }
+        });
+
+//        Intent triggeredIntent = getStartedActivityIntent();
+//        assertNotNull("Intent should have triggered after button press", triggeredIntent);
+//        String data = triggeredIntent.getExtras().getString("result");
+//        assertEquals("Incorrect result data passed via the intent", "Testing Text", data);
+
+    }
+
 }
+
