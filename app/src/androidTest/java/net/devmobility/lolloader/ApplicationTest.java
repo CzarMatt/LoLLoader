3710ef0191b2a080ead697eec7886396f22f7d53
Matt Mossman <czarmatt@gmail.com>
2015-05-08 09:20:40 -0700

    Minor bug and stability fixes.

--- app/src/androidTest/java/net/devmobility/lolloader/ApplicationTest.java
+++ app/src/androidTest/java/net/devmobility/lolloader/ApplicationTest.java
@@ -5,6 +5,11 @@ import android.test.ActivityInstrumentationTestCase2;
 import android.test.suitebuilder.annotation.SmallTest;
 import android.widget.ImageButton;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.Volley;
+
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
@@ -37,14 +42,34 @@ public class ApplicationTest extends ActivityInstrumentationTestCase2<MainActivi
 
         final ImageButton button = (ImageButton) activity.findViewById(R.id.imagebutton);
         assertNotNull("This image button shouldn't be null", button);
-        activity.runOnUiThread(new Runnable() {
+//        activity.runOnUiThread(new Runnable() {
+//            @Override
+//            public void run() {
+//                button.performClick();
+//            }
+//        });
+
+        initialize();
+
+    }
+
+    private void initialize() {
+        RequestQueue queue = Volley.newRequestQueue(getActivity());
+        Response.Listener<String> listener = new Response.Listener<String>() {
             @Override
-            public void run() {
-                button.performClick();
+            public void onResponse(String response) {
+                assertNotNull(response);
+                int i = Integer.parseInt(response);
+                assertTrue(i>0);
+            }
+        };
+        LolCatTotalRequest r = new LolCatTotalRequest("lolcat", listener, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                assertNotNull(error);
             }
         });
-
-
+        queue.add(r);
     }
 
 }
